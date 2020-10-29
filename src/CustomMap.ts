43823b7a0e6546de4288ecf0b937973f795c6876
Julia Tan <juliajtan@gmail.com>
2020-10-29 17:50:53 +0000

    Add info window to each marker on click

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -22,12 +22,19 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: 'Hi there',
+      });
+      infoWindow.open(this.googleMap, marker);
     });
   }
 }
