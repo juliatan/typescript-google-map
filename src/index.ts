5fa16ee303cf850948da567d9f59eab5bd0a436c
Julia Tan <juliajtan@gmail.com>
2020-10-29 12:10:42 +0000

    Implement Google Maps API and render map to index.html

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,12 @@ console.log(user);
 
 const company = new Company();
 console.log(company);
+
+// Comes from the Google Maps script file in index.html. Can access as global var because we installed @types/googlemaps.
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
